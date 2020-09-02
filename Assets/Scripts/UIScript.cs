53540e0258d3d601a45f792ec790e04a9fe16073
appels <appels@telenet.be>
2020-09-02 17:14:59 +0200

    Before audio spatializer update

---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+
+    public GameObject btn_Red, btn_Green, btn_Blue, img_Colors, UISound;
+
+    private void Start()
+    {
+        btn_Red.GetComponent<Button>().onClick.AddListener(call: btn_Red_Click);
+        btn_Green.GetComponent<Button>().onClick.AddListener(call: btn_Green_Click);
+        btn_Blue.GetComponent<Button>().onClick.AddListener(call: btn_Blue_Click);
+    }
+
+    private void btn_Red_Click()
+    {
+        img_Colors.GetComponent<Image>().color = Color.red;
+        UISound.GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sounds/Spacey Click_Minimal UI Sounds"));
+    }
+
+    private void btn_Green_Click()
+    {
+        img_Colors.GetComponent<Image>().color = Color.green;
+        UISound.GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sounds/Spacey Click_Minimal UI Sounds"));
+    }
+
+    private void btn_Blue_Click()
+    {
+        img_Colors.GetComponent<Image>().color = Color.blue;
+        UISound.GetComponent<AudioSource>().PlayOneShot(Resources.Load<AudioClip>("Sounds/Spacey Click_Minimal UI Sounds"));
+    }
+}
